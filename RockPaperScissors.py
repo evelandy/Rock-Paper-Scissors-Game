7eec8545f1187df47e2ca9b9f42245df01b575f3
William Griffin <40611326+evelandy@users.noreply.github.com>
2018-09-29 15:45:20 -0500

    Rock Paper Scissors game
    A Quick game of Rock Paper Scissors written in Python3

---
+++ RockPaperScissors.py
@@ -0,0 +1,86 @@
+import random
+import time
+import os
+
+
+CHOICES = ['Rock', 'Paper', 'Scissors']
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+while True:
+    print("To quit press (q)")
+    user_choice = input("Rock, Paper or Scissors? ")
+    pc_choice = random.choice(CHOICES)
+
+    if user_choice.lower() == 'q':
+        print("Thank You For Playing Come Back Soon!")
+        break
+
+    print("Ready")
+    time.sleep(1)
+    print("Set")
+    time.sleep(1)
+    print("Go!")
+    print("You Chose: {} \nPC Chose: {}".format(user_choice, pc_choice))
+
+    if user_choice.lower() == pc_choice.lower():
+        print("Tie!")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    elif user_choice.lower() == 'rock' and pc_choice.lower() == 'scissors':
+        print("You Win!")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    elif user_choice.lower() == 'paper' and pc_choice.lower() == 'rock':
+        print("You Win!")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    elif user_choice.lower() == 'scissors' and pc_choice.lower() == 'paper':
+        print("You Win!")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    elif user_choice.lower() == 'rock' and pc_choice.lower() == 'paper':
+        print("You loose...")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    elif user_choice.lower() == 'paper' and pc_choice.lower() == 'scissors':
+        print("You loose...")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    elif user_choice.lower() == 'scissors' and pc_choice.lower() == 'rock':
+        print("You loose...")
+        answer = input("Would You Like To Play Again? (y/n)")
+        if answer.lower() == 'n':
+            print("See You Next Time!")
+            break
+        else:
+            clear()
+    else:
+        print("Sorry You Didn't Choose Rock, Paper, Or Scissors... Try again...")
